bc4ad1f37963bb1bd63b17c086ebfdede2b11cf1
Thomas Volk <thomas.volk@trustedshops.de>
2025-06-06 13:39:16 +0200

    update

--- playground/test/test_playground.ml
+++ playground/test/test_playground.ml
@@ -4,8 +4,8 @@ let rec loop f c = function
   | 0 -> ()
   | n ->
     write_file (c ^ (string_of_int n)) f;
-    let c = read_file f in
-    print_endline ( (string_of_int n) ^ ": " ^ c);
+    let c' = read_file f in
+    print_endline ( (string_of_int n) ^ ": " ^ c');
     loop f c (n - 1)
 
 let () = 
